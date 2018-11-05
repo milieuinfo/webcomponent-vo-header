38149f479398d1df6ca01c5f8779db994b9fbd32
Tom Coemans <tom.coemans@gmail.com>
2018-11-05 15:19:42 +0100

    GVR-39 pad problemen opgelost

--- vo-header.js
+++ vo-header.js
@@ -1,5 +1,5 @@
 import { LitElement, html } from "./assets/@polymer/lit-element/lit-element.js";
-import './node_modules/vo-merklogo/vo-merklogo.js';
+import './assets/vo-merklogo/vo-merklogo.js';
 
 /**
  * `vo-header`
